87739ab4fc75ff576b353e5908ba40b22608f1db
Adam <raqsasg@gmail.com>
2021-06-24 21:50:33 +0200

    fix: Accept only ISO_8601 date strings

--- src/events/helpers/date.ts
+++ src/events/helpers/date.ts
@@ -1,12 +1,19 @@
 import { InvalidDateError } from '../errors/InvalidDate';
 
+const DATE_ISO_8601_PATTERN = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
+
 function validateDateString(date: string) {
-  const isDateValid = !!Date.parse(date);
-  if (isDateValid) {
-    return;
+  const isValidDate = !!Date.parse(date);
+  if (!isValidDate) {
+    throw new InvalidDateError();
   }
 
-  throw new InvalidDateError();
+  const isValidISODate = date.match(DATE_ISO_8601_PATTERN);
+  if (!isValidISODate) {
+    throw new InvalidDateError();
+  }
+
+  return;
 }
 
 function isDateInRange(date: Date, start: Date, end: Date) {
